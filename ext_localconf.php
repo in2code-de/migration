4262957081f85cce5acb4fdc446de1f822c21d23
Alex Kellner <alexander.kellner@in2code.de>
2019-02-08 11:11:27 +0100

    [TASK] Remove not correctly working old ImportExportCommandController with impexp and datahandler functionality

--- ext_localconf.php
+++ ext_localconf.php
@@ -15,8 +15,6 @@ call_user_func(
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] =
             \In2code\Migration\Command\HelpCommandController::class;
         $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] =
-            \In2code\Migration\Command\ImportExportCommandController::class;
-        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] =
             \In2code\Migration\Command\PortCommandController::class;
 
         unset($extKey);
