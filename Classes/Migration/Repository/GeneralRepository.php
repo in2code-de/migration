in2code-de/migration

commit 5cd8d7dc22df8730c56e29087a945e50bed4cc76

[BUGFIX] Migration: Don't expect a deleted field for every table
But do a check if field deleted is really existing

Classes/Migration/Repository/GeneralRepository.php
@@ -131,7 +131,10 @@ class GeneralRepository
      */
     protected function getWhereClause(string $tableName, string $additionalWhere): string
     {
-        $whereClause = 'deleted=0';
+        $whereClause = '';
+        if (DatabaseUtility::isFieldExistingInTable('deleted', $tableName)) {
+            $whereClause .= 'deleted=0';
+        }
         $whereClause = $this->getWhereClauseForLimitToRecord($whereClause, $tableName);
         $whereClause = $this->getWhereClauseForLimitToPage($whereClause, $tableName);
         if ($this->enforce === false && DatabaseUtility::isFieldExistingInTable('_migrated', $tableName)) {

commit edf53f660ffdf40a1d841481db0c076c7207f7f5

[BUGFIX] Fix sql regression from 5cd8d7dc22df8730c56e29087a945e50bed4cc76

Classes/Migration/Repository/GeneralRepository.php
@@ -131,9 +131,9 @@ class GeneralRepository
      */
     protected function getWhereClause(string $tableName, string $additionalWhere): string
     {
-        $whereClause = '';
+        $whereClause = '1';
         if (DatabaseUtility::isFieldExistingInTable('deleted', $tableName)) {
-            $whereClause .= 'deleted=0';
+            $whereClause = 'deleted=0';
         }
         $whereClause = $this->getWhereClauseForLimitToRecord($whereClause, $tableName);
         $whereClause = $this->getWhereClauseForLimitToPage($whereClause, $tableName);