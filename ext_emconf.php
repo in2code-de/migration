d89610852e963cdb3ff5fa17e2ef3b368ee49d74
Alex Kellner <alexander.kellner@in2code.de>
2020-05-04 15:14:02 +0200

    [TASK] Update dependencies to TYPO3 9 or 10

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = [
     ],
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-9.99.99'
+            'typo3' => '9.5.0-10.4.99'
         ],
         'conflicts' => [],
         'suggests' => [],
