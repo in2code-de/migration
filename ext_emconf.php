3f73fc5cf1837140e79cfeadb8ffee2018faba11
Devid Messner <devid.messner@konverto.eu>
2021-09-20 16:51:41 +0200

    fix: replace deprecated Commands.php with service.yaml

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,7 +19,7 @@ $EM_CONF[$_EXTKEY] = [
     ],
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99'
+            'typo3' => '9.5.0-11.5.99'
         ],
         'conflicts' => [],
         'suggests' => [],
