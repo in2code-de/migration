415482899dbc699ef8135a3278ce4e5c1018b9b8
Alexander Kellner <alexander.kellner@in2code.de>
2025-04-29 15:34:37 +0200

    [TASK] Release preparations

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,7 +9,7 @@ $EM_CONF[$_EXTKEY] = [
     'dependencies' => 'extbase, fluid',
     'state' => 'stable',
     'author_company' => 'in2code GmbH',
-    'version' => '12.3.1',
+    'version' => '12.4.0',
     'autoload' => [
         'psr-4' => ['In2code\\Migration\\' => 'Classes'],
     ],
