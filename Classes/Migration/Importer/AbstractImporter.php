3081b371d4b811b23ea1fed581731216ed5957fd
Alexander Kellner <alexander.kellner@in2code.de>
2025-05-06 10:36:17 +0200

    [TASK] Show class name if misconfiguration exception is thrown

--- Classes/Migration/Importer/AbstractImporter.php
+++ Classes/Migration/Importer/AbstractImporter.php
@@ -231,7 +231,10 @@ abstract class AbstractImporter
         foreach ($this->propertyHelpers as $propertyName => $helperConfigurations) {
             foreach ($helperConfigurations as $key => $helperConfiguration) {
                 if (is_int($key) === false) {
-                    throw new ConfigurationException('Misconfiguration of your importer class', 1569574630);
+                    throw new ConfigurationException(
+                        'Misconfiguration of your importer class ' . static::class,
+                        1569574630
+                    );
                 }
                 if (class_exists($helperConfiguration['className']) === false) {
                     throw new ConfigurationException(
--- Classes/Migration/Migrator/AbstractMigrator.php
+++ Classes/Migration/Migrator/AbstractMigrator.php
@@ -174,7 +174,10 @@ abstract class AbstractMigrator
         foreach ($this->propertyHelpers as $propertyName => $helperConfigurations) {
             foreach ($helperConfigurations as $key => $helperConfiguration) {
                 if (is_int($key) === false) {
-                    throw new ConfigurationException('Misconfiguration of your migrator class', 1569574630);
+                    throw new ConfigurationException(
+                        'Misconfiguration of your migrator class ' . static::class,
+                        1569574630
+                    );
                 }
                 if (class_exists($helperConfiguration['className']) === false) {
                     throw new ConfigurationException(
