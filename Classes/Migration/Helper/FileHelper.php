6f2d265a06207e1d0ffa69c8202a65779e7a2b18
Alexander Kellner <alexander.kellner@in2code.de>
2025-06-05 16:30:32 +0200

    [BUGFIX] Remove old column from query sys_file_reference.table_local

--- Classes/Migration/Helper/FileHelper.php
+++ Classes/Migration/Helper/FileHelper.php
@@ -208,7 +208,6 @@ class FileHelper implements SingletonInterface
             'uid_foreign' => $recordIdentifier,
             'tablenames' => $tableName,
             'fieldname' => $fieldName,
-            'table_local' => 'sys_file',
         ];
         return $databaseHelper->createRecord('sys_file_reference', $additionalProperties + $properties);
     }
