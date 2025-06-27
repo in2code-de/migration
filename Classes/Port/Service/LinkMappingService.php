in2code-de/migration

commit 26ff0591ef3a50d4464abfc5ddabc7b11ae37e8f

[NEW] Update rte links during import, when records on a page are linked, and map to the records on the imported page, so sections links are fully migrated after import

Classes/Port/Service/LinkMappingService.php
@@ -234,10 +234,11 @@ class LinkMappingService
     protected function updatePageLinks(string $value): string
     {
         return preg_replace_callback(
-            '~(t3://page\?uid=)(\d+)~',
+            '~(t3://page\?uid=)(\d+)(#?\d*)~',
             [$this, 'updatePageLinksCallback'],
             $value
         );
+
     }
 
     /**
@@ -317,7 +318,7 @@ class LinkMappingService
     }
 
     /**
-     * Replace t3://page?uid=123 => t3://page?uid=234
+     * Replace t3://page?uid=123#567 => t3://page?uid=234#789
      *
      * @param array $match
      * @return string
@@ -326,7 +327,15 @@ class LinkMappingService
     {
         $oldPageIdentifier = (int)$match[2];
         $newPageIdentifier = $this->mappingService->getNewPidFromOldPid($oldPageIdentifier);
-        return $match[1] . $newPageIdentifier;
+
+        // Check if, #section Reference is given
+        $hashValue = '';
+        if(isset($match[3])){
+            $sectionOldPageIdentifier = (int)$match[3];
+            $hashValue =  $this->mappingService->getNewPidFromOldPid($sectionOldPageIdentifier);
+        }
+
+        return $match[1] . $newPageIdentifier . $hashValue;
     }
 
     /**

commit 4a24bdd5b864f9babcb23077f9c6a77d3cbfe609

[BUGFIX] Fix page relations with sections for imports
As a regression from 26ff0591ef3a50d4464abfc5ddabc7b11ae37e8f

Classes/Port/Service/LinkMappingService.php
@@ -234,7 +234,7 @@ class LinkMappingService
     protected function updatePageLinks(string $value): string
     {
         return preg_replace_callback(
-            '~(t3://page\?uid=)(\d+)(#?\d*)~',
+            '~(t3://page\?uid=)(\d+)(#c\d+)?~',
             [$this, 'updatePageLinksCallback'],
             $value
         );
@@ -330,8 +330,10 @@ class LinkMappingService
         // Check if, #section Reference is given
         $section = '';
         if (($match[3] ?? '') !== '') {
-            $contentIdentifier = (int)substr($match[3], 1);
-            $section = '#' . $this->mappingService->getNewFromOld($contentIdentifier, 'tt_content');
+            $contentIdentifier = (int)ltrim($match[3], '#c');
+            if ($contentIdentifier > 0) {
+                $section = '#c' . $this->mappingService->getNewFromOld($contentIdentifier, 'tt_content');
+            }
         }
 
         return $match[1] . $newPageIdentifier . $section;